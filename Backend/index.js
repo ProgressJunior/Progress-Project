06a8496eea7758ab10ee2bcebdeee2f7ae4f5c62
Gavaii <61867970+Gavaii@users.noreply.github.com>
2022-03-21 18:24:31 +0100

    🐛 Fixed Connection Bug and Dotenv

--- Backend/index.js
+++ Backend/index.js
@@ -1,61 +1,48 @@
 const express = require("express");
 const app = express();
-const port = 3030;
+const express_port = 3030;
 require("dotenv").config();
 const sql = require("mssql");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/:arg", (req, res) => {
+  req.param.arg;
+  res.send("Hello World!");
+});
+
+app.listen(express_port, () => {
+  console.log(`Example app listening on port ${express_port}`);
+});
 
 const sqlConfig = {
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   server: process.env.SERVER,
-  port: process.env.PORT,
+  port: 50915,
   options: {
     encrypt: false, // for azure
     trustServerCertificate: true, // change to true for local dev / self-signed certs
   },
 };
+sql.connect(sqlConfig, (err) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+  console.log("Connection Successful!");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/updateTable/:arg', (req, res) => {
-  queryDatabase(req.params.arg)
-})
-
-function queryDatabase(query){
-  sql.connect(sqlConfig, (err) => {
-    if (err) {
-      throw err;
-    }
-    console.log("Connection Successful !");
-    //"select * from dbo.LocPalHistory"
-    new sql.Request().query(query, (err, result) => {
-      console.dir(result);
-    });
+  new sql.Request().query("select * from dbo.LocPalHistory", (err, result) => {
+    //handle err
+    console.dir(result);
+    // This example uses callbacks strategy for getting results.
   });
-  res.end();
-}
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-// sql.connect(sqlConfig, (err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log("Connection Successful !");
-
-//   new sql.Request().query("select * from dbo.LocPalHistory", (err, result) => {
-//     //handle err
-//     console.dir(result);
-//     // This example uses callbacks strategy for getting results.
-//   });
-// });
-
-// sql.on("error", (err) => {
-//   // ... error handler
-//   console.log("Sql database connection error ", err);
-// });
+sql.on("error", (err) => {
+  // ... error handler
+  console.log("Sql database connection error ", err);
+});
