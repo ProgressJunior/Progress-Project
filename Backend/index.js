350599bf662de4c39025edf99d7c83754b607fe4
Kurisu003 <dani003@outlook.com>
2022-03-21 14:56:01 +0100

    update

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
 const sql = require("mssql");
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/:arg', (req, res) => {
+  req.param.arg;
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 const sqlConfig = {
   user: "userTeam1",
   password: "KennwortTeam1",
