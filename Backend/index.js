4957508ff20cf04fb33b6fefc5f72313dd8994ab
Gavaii <61867970+Gavaii@users.noreply.github.com>
2022-03-22 09:53:16 +0100

    🎨 Moved Ports to dotenv file

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const express_port = 3030;
 require("dotenv").config();
 const sql = require("mssql");
 
@@ -9,13 +8,12 @@ const sql = require("mssql");
         SQL
 
 */
-
 const sqlConfig = {
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
   server: process.env.SERVER,
-  port: 50915,
+  port: parseInt(process.env.PORT),
   options: {
     // encrypt: false, // for azure
     trustServerCertificate: true, // change to true for local dev / self-signed certs
@@ -24,11 +22,11 @@ const sqlConfig = {
 
 // Function that connects to and Database
 // and then queries the passed parameter
-function queryDatabase(query){
-
+function queryDatabase(query) {
   // Establishes connection to database
   sql.connect(sqlConfig, (err) => {
     if (err) {
+      console.log(err);
       throw err;
     }
 
@@ -44,23 +42,23 @@ sql.on("error", (err) => {
   console.log("Sql database connection error ", err);
 });
 
-
-
 /*
 
         EXPRESS
 
 */
+const express_port = process.env.EXPRESS_PORT;
+
 app.listen(express_port, () => {
   console.log(`Example app listening on port ${express_port}`);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/updateTable/:arg', (req, res) => {
+app.get("/updateTable/:arg", (req, res) => {
   // res.send(req.params.arg)
-  queryDatabase(req.params.arg)
+  queryDatabase(req.params.arg);
   res.end();
-})
+});
