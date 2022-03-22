dedec55fdd309e63e41a8dbff85969ae72e1296f
Kurisu003 <dani003@outlook.com>
2022-03-22 10:02:25 +0100

    Started implementation of Frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  function queryFunction(){
+  fetch('http://localhost:3030/updateTable/' + document.getElementById("queryInput").value)
+        .then(response => response.json())
+        .then(data => this.setState({ totalReactPackages: data.total }));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Test</p>
+      <input id="queryInput"></input>
+      <button onClick={()=>{queryFunction()}} >test</button>
     </div>
   );
 }
