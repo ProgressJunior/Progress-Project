c530639276ce3cecedc7f16366390ee042bd5174
Kurisu003 <dani003@outlook.com>
2022-03-23 10:38:48 +0100

    🐥Implemented Datepicker in frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,53 @@
+// import { LocalizationProvider } from '@material-ui/pickers';
+// import MomentUtils from 'moment';
+
+// function App() {
+
+//   function queryFunction(){
+//   fetch('http://localhost:3030/updateTable/' + document.getElementById("queryInput").value)
+//         .then(response => response.json())
+//         .then(data => this.setState({ totalReactPackages: data.total }));
+//   }
+
+//   // var moment = require('moment');
+//   // let temp = moment().format();
+//   // console.log(temp);
+
+//   return (
+//     <div className="App">
+//       <LocalizationProvider dateAdapter={MomentUtils}>
+//         <p>Test</p>
+//         <input id="queryInput"></input>
+//         <button onClick={()=>{queryFunction()}} >test</button>
+//       </LocalizationProvider>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+import { TextField } from '@mui/material';
+import React, { useState } from 'react';
 
 function App() {
-
-  function queryFunction(){
-  fetch('http://localhost:3030/updateTable/' + document.getElementById("queryInput").value)
-        .then(response => response.json())
-        .then(data => this.setState({ totalReactPackages: data.total }));
-  }
+  const [date, setDate] = useState(Date.now);
   return (
-    <div className="App">
-      <p>Test</p>
-      <input id="queryInput"></input>
-      <button onClick={()=>{queryFunction()}} >test</button>
-    </div>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <DatePicker
+        label="Basic example"
+        value={date}
+        onChange={(newValue) => {
+          setDate(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      {/* display date */}
+      <p>test: {}</p>
+    </LocalizationProvider>
   );
 }
 
