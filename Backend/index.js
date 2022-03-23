750b3e1f2051d47624c14b5363c4de4d5c9d9a22
Gavaii <61867970+Gavaii@users.noreply.github.com>
2022-03-23 15:37:28 +0100

    🚑 added hardcoded qv_index

--- Backend/index.js
+++ Backend/index.js
@@ -23,28 +23,26 @@ const sqlConfig = {
 
 // Function that connects to Database
 // and then queries the passed parameter
-async function connect(){
+async function connect() {
   // connect
   try {
     // make sure that any items are correctly URL encoded in the connection string
-    await sql.connect(sqlConfig)
+    await sql.connect(sqlConfig);
   } catch (err) {
     // ... error checks
   }
 }
 
-async function query(query){
+async function query(query) {
   //await new Promise(r => setTimeout(r, 2000));
-  return await sql.query`${query}`
-  console.dir(result)
+  return await sql.query`${query}`;
+  console.dir(result);
 }
 
 async function main() {
-
   await connect();
-  let res = await sql.query`SELECT * FROM LocPalHistory`
-  console.log(res)
-    
+  let res = await sql.query`SELECT * FROM LocPalHistory`;
+  console.log(res);
 }
 main();
 
@@ -86,49 +84,88 @@ function genQuery(taktplatz, palette, duration, date) {
 }
 
 let path = [
-  "TP 1",    "TP 2",           "QV 2",
-  "TP 3",    "TP 4",           "QV 1",
-  "TP 5",    "TP 6",           "QV 3",
-  "TP 10",                     "QV 8",
-  "TP 9",    "TP 11",          "QV 4",
-  "TP 12",   "TP 13", "TP 14", "QV 7",
-  "TP 14.1", "TP 15",          "QV 5",
-  "TP 17",                     "QV 6",
-  "TP 18",   "TP 23", "TP 25",
-]
-
+  "TP 1",
+  "TP 2",
+  "QV 2",
+  "TP 3",
+  "TP 4",
+  "QV 1",
+  "TP 5",
+  "TP 6",
+  "QV 3",
+  "TP 10",
+  "QV 8",
+  "TP 9",
+  "TP 11",
+  "QV 4",
+  "TP 12",
+  "TP 13",
+  "TP 14",
+  "QV 7",
+  "TP 14.1",
+  "TP 15",
+  "QV 5",
+  "TP 17",
+  "QV 6",
+  "TP 18",
+  "TP 23",
+  "TP 25",
+];
 
 // Pseudocode to write Data to palette
 let currentePaletteId = 2;
-path.forEach((e)=>{
-  e.startsWith("Q") ? console.log(genQuery(e, currentePaletteId, 2, date)) : console.log(genQuery(e, currentePaletteId, 1, date))
-  
-  if(e.equals("TP 5")){
-    queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (RemovedFromPalUnit)")
-  }
-  else if(e.equals("TP 6")){
-    if(queryDatabase("select PalNo from dbo.PalDataBelHistory where PalNo = " + currentePaletteId) is empty){
-      queryDatabase("insert into dbo.PalData (ProdSeqId) values ("+currentePaletteId+")")
-      queryDatabase("insert into dbo.PalDataBelHistory (PalNo, TimeStamp) values ("+currentePaletteId+","+time+")")
-      queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (ShutteringFinished)")
-    }
-  }
-  else if(e.equals("TP 12")){
-    queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (BarsPlaced)")
-  }
-  else if(e.equals("TP 13")){
-    queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (GirdersPlaced)")
-  }
-  else if(e.equals("TP 23")){
-    queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (ConcretingFinished)")
-  }
-  // LG bei Path einfuegen
-  // Lagerplatz bestimmen
-  else if(e.equals("LG 1")){
-    queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (EnteredInDryingChamber)")
-  }
-  // Finished Variable bei Path einfuegen
-  else if(e.equals("Vaffanculo")){
-    queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (RemovedFromDryingChamber)")
-  }
-})
+// path.forEach((e)=>{
+//   e.startsWith("Q") ? console.log(genQuery(e, currentePaletteId, 2, date)) : console.log(genQuery(e, currentePaletteId, 1, date))
+
+//   if(e.equals("TP 5")){
+//     queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (RemovedFromPalUnit)")
+//   }
+//   else if(e.equals("TP 6")){
+//     if(queryDatabase("select PalNo from dbo.PalDataBelHistory where PalNo = " + currentePaletteId) is empty){
+//       queryDatabase("insert into dbo.PalData (ProdSeqId) values ("+currentePaletteId+")")
+//       queryDatabase("insert into dbo.PalDataBelHistory (PalNo, TimeStamp) values ("+currentePaletteId+","+time+")")
+//       queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (ShutteringFinished)")
+//     }
+//   }else if(e.equals("TP 12")){
+//     queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (BarsPlaced)")
+//   }
+//   else if(e.equals("TP 13")){
+//     queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (GirdersPlaced)")
+//   }
+//   else if(e.equals("TP 23")){
+//     queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (ConcretingFinished)")
+//   }
+//   // LG bei Path einfuegen
+//   // Lagerplatz bestimmen
+//   else if(e.equals("LG 1")){
+//     queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (EnteredInDryingChamber)")
+//   }
+//   // Finished Variable bei Path einfuegen
+//   else if(e.equals("Vaffanculo")){
+//     queryDatabase("insert into dbo.PalDataMilestoneHistory (PalUnitAssigned) values (RemovedFromDryingChamber)")
+//   }
+// })
+
+var qv_index = {
+  "TP 2": 1,
+  "TP 3": 2,
+  "TP 4": 2,
+  "TP 5": 3,
+  "TP 6": 1,
+  "TP 7": 2,
+  "TP 8": 3,
+  "TP 9": 2,
+  "TP 10": 1,
+  "TP 11": 1,
+  "TP 12": 2,
+  "TP 14": 1,
+  "TP 14.1": 2,
+  "TP 15": 1,
+  "TP 16": 2,
+  "TP 16.1": 3,
+  "TP 17.1": 4,
+  "TP 17.2": 1,
+  "TP 18": 2,
+  "TP 19": 3,
+  "TP 20": 4,
+};
